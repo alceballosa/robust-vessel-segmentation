5069c8d6ef72f4afc00c5f6e874217fa16c82fda
Alberto Mario Ceballos-Arroyo <alceballosa@gmail.com>
2026-02-26 20:21:01 -0500

    Fix run scripts

--- run_segmentation_241.sh
+++ run_segmentation_241.sh
@@ -2,4 +2,4 @@
 
 export version=241
 export PATH=./atlases_and_weights/ants-2.6.3/bin:$PATH
-python ./src/inference/extractVessels.py -d ${1} ${2}  -m 'Prediction' -s 0.5 -g ${3} -v ${version}
+python ./scripts/inference/extractVessels.py -d ${1} ${2}  -m 'Prediction' -s 0.5 -g ${3} -v ${version}
